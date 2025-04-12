63e7bb6d193330d3c93589e9b4ab6bdc56f67ff6
lexuv2 <alex2003gd@gmail.com>
2025-04-12 18:15:19 +0200

    poc

---
+++ visualziers/godot/kropkara-godot/main_window.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _ready() -> void:
+	pass
+
+func _on_button_pressed() -> void:
+	Caller.on_click() 
+	pass # Replace with function body.
--- visualziers/godot/kropkara-rust/src/lib.rs
+++ visualziers/godot/kropkara-rust/src/lib.rs
@@ -1,11 +1,12 @@
 mod caller;
 mod map;
+mod image_generator;
 use godot::prelude::*;
 
 
 struct KropkaraRust;
 
-
+#[gdextension]
 unsafe impl ExtensionLibrary for KropkaraRust {}
 
 
